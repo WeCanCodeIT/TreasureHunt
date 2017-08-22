652706f8e05202d190b0a283bc18c21d8224d28a
Your Name <you@example.com>
2017-08-22 10:02:38 -0400

    setup array locations

---
+++ TreasureHunt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreasureHunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Locations = { 1234, 7396, 4578, 8125 };
+            
+            //Do Something
+
+        }
+    }
+}
